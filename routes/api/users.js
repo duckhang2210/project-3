ea3fa8cb3eefce5ce2011f3f1c131ee1d885b85c
Duc Khang Nguyen <duckhang2210@gmail.com>
2019-11-16 22:10:36 -0600

    edit users api and add validations when register

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,10 +2,30 @@
 
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// #route   GET api/users
-// #for     Testing route
+// #route   POST api/users
+// #for     Register user
 // #access  Public (no token needed)
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please enter your valid EMAIL').isEmail(),
+    check(
+      'password',
+      'Your password must contain AT LEAST 5 characters'
+    ).isLength({ min: 5 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User route');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //Connect DB
 connectDB();
 
+//Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API is running'));
 
 //Define Routes
