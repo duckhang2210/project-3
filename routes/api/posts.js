e165fec09be215b2a80ba77f3464bfd2a85a9e5f
Duc Khang Nguyen <duckhang2210@gmail.com>
2019-11-17 21:57:03 -0600

    like and unlike post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -95,4 +95,57 @@ router.delete('/:id', auth, async (req, res) => {
   }
 });
 
+// #route   PUT api/posts/like/:id
+// #for     Like post
+// #access  Private
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //CHeck if post already been liked by user
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).json({ message: 'You already liked this post' });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+
+    res.json(post.likes);
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+// #route   PUT api/posts/unlike/:id
+// #for     Unlike post
+// #access  Private
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //CHeck if post already been liked by user
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length ===
+      0
+    ) {
+      return res
+        .status(400)
+        .json({ message: 'You has not like this post yet' });
+    }
+
+    const removeIndex = post.likes
+      .map(like => like.user.toString())
+      .indexOf(req.user.id);
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 module.exports = router;
