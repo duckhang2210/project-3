1a8e0db81317d1051c3f5791c695245965e50d45
Duc Khang Nguyen <duckhang2210@gmail.com>
2019-11-20 22:03:29 -0600

    see specific post

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import EditProfile from './components/profile-form/EditProfile';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 //Redux
@@ -54,6 +55,7 @@ const App = () => {
                 component={EditProfile}
               />
               <PrivateRoute exact path='/posts' component={Posts} />
+              <PrivateRoute exact path='/posts/:id' component={Post} />
             </Switch>
           </section>
         </Fragment>
